30c7dc68038ae064c56bbdfec51475035fd5c81c
zazila1 <zazila1@gmail.com>
2020-08-10 02:51:39 +0300

    arena, soldier model and some scripts

---
+++ Assets/Scripts/Models/SoldierParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SoldierParams
+{
+    public float _Speed;
+    public float _AttackRange;
+    public float _Damage;
+}
---
+++ Assets/Scripts/Soldier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour, IDamagable
+{
+    [SerializeField] private SoldierParams _SoldierParams;
+    
+    public void GetDamage(int damageValue)
+    {
+        throw new System.NotImplementedException();
+    }
+}
